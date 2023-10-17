aaf5284852bf3fb98dce8425b07291f4af7846db
JonathanMabiala <62263731+JonathanMabiala@users.noreply.github.com>
2023-10-17 06:40:20 -0400

    Server initial code

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("server is working....");
+import express from "express";
+
+const app = express();
+const PORT = 5100;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(PORT, () => {
+  console.log("server running...");
+});
