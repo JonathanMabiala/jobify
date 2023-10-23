43c3d243c99523e54d96e64fd56065f6593fcbb5
JonathanMabiala <62263731+JonathanMabiala@users.noreply.github.com>
2023-10-23 01:18:13 -0400

    Implemented Toastify for cool notifications

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,15 +2,17 @@ import { Form, redirect, useNavigation, Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
 import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
 
   try {
     await customFetch.post("/auth/register", data);
+    toast.success("Registration successful");
     return redirect("/login");
   } catch (error) {
-    console.log(error);
+    toast.error(error?.response?.data?.msg);
     return error;
   }
 };
@@ -41,7 +43,7 @@ const Register = () => {
           <FormRow type="password" name="password" defaultValue="secret123" />
         </div>
         <button type="submit" className="btn btn-block" disabled={isSubmitting}>
-          {isSubmitting ? 'submitting...' : 'submit'}
+          {isSubmitting ? "submitting..." : "submit"}
         </button>
         <p>
           Already a member?
